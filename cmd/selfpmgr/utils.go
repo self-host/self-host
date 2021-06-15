d9687b5c133cec9966baf70d6c3a2e5ca052cd3b
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2021-06-15 10:00:07 +0200

    go fmt

--- cmd/selfpmgr/utils.go
+++ cmd/selfpmgr/utils.go
@@ -1,10 +1,10 @@
 package main
 
 func StringSliceIndex(haystack []string, needle string) int {
-        for idx, a := range haystack {
-                if a == needle {
-                        return idx
-                }
-        }
-        return -1
+	for idx, a := range haystack {
+		if a == needle {
+			return idx
+		}
+	}
+	return -1
 }
