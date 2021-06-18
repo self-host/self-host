ef00435a1676a299125ca866030c51eddfdb5853
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2021-06-18 09:16:29 +0200

    Smaller header

--- api/aapije/alert.go
+++ api/aapije/alert.go
@@ -1,20 +1,6 @@
-/*
-Copyright (C) 2021 The Self-host Authors.
-This file is part of Self-host <https://github.com/self-host/self-host>.
-
-Self-host is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Self-host is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Self-host.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2021 The Self-host Authors. All rights reserved.
+// Use of this source code is governed by the GPLv3
+// license that can be found in the LICENSE file.
 
 package aapije
 
